0b783e3c9b6ced7c50930d0923278f8f9f49b8d3
Lazo07 <noreenlazo07@gmail.com>
2018-05-27 05:31:56 +0800

    About section: replaced lorem ipsum

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,8 +18,10 @@
 <nav>
     <h1>About</h1>
     <span>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </span>
+       
+Needing to pee but just can't find the place to do so without giving you sanitary nightmare? ToiletBook is an iOS mobile app dedicated to find the cleanest and most accessible washrooms for you based on user reviews and location.         
+        <!--Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. -->
+    </span>
     <a href="#">
         <div id="dl">
             <br>
