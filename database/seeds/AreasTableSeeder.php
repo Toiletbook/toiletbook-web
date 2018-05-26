5b369f1be02adf68c738e666cc252b2b7b2d3fd6
Batur <batur@kfmc.ph>
2018-05-26 14:54:59 +0800

    data: add area seeder

---
+++ database/seeds/AreasTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AreasTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('areas')->insert(
+            [
+                'name' => 'Makati',
+            ],
+            [
+                'name' => 'BGC',
+            ]
+        );
+    }
+}
