927a9d4e42a594fb56fa116b449444cf2cf4256b
awkwardusername <awkwardusername@gmail.com>
2018-05-27 12:55:24 +0800

    screenies

---
+++ resources/views/screenshots.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <!-- meta -->
+    <title>ToiletBook</title>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="css/layout.css"/>
+    <link rel="stylesheet" type="text/css" href="css/design.css"/>
+    <style>
+        #ss {
+            margin-top: 20px;
+            text-align: center;
+        }
+
+        #ss img {
+            width: 25%;
+            margin-right: 10px;
+            margin-top: 20px;
+        }
+
+        #background img {
+            opacity: 0.5;
+        }
+    </style>
+
+</head>
+<body>
+<div id="ss">
+    <img src="/img/ss/1.jpg"></img>
+    <img src="/img/ss/2.jpg"></img>
+    <img src="/img/ss/3.jpg"></img>
+    <!-- -->
+    <img src="/img/ss/4.jpg"></img>
+    <img src="/img/ss/5.jpg"></img>
+    <img src="/img/ss/6.jpg"></img>
+    <!-- -->
+    <img src="/img/ss/7.jpg"></img>
+    <img src="/img/ss/8.jpg"></img>
+    <img src="/img/ss/9.jpg"></img>
+</div>
+<div id="background">
+    <img></img>
+</div>
+<footer>
+    Copyright © 2018 <a href="https://github.com/Toiletbook">ToiletBook</a> | All Rights Reserved
+</footer>
+</body>
+</html>
